36a8daf5a1b553a75555fe0155017693cd6fedfb
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-06 21:15:59 -0700

    websocket multiplayer server starter, pass user positions

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log("lol")
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({
+    port: 80
+});
+
+const users: any = {};
+
+wss.on('connection', (ws) => {
+    let userId = "";
+
+    ws.on('message', (data) => {
+        const message = JSON.parse(data.toString());
+        console.log(message);
+        switch (message.type) {
+            case "position": {
+                userId = message.userId;
+                users[message.userId] = {
+                    x: message.x,
+                    y: message.y,
+                    z: message.z
+                };
+                break;
+            }
+            default:
+                break;
+        }
+    });
+
+    ws.on("close", () => {
+        delete users[userId]
+    })
+    setInterval(() => {
+        ws.send(JSON.stringify({
+            type: "users",
+            users
+        }));
+    }, 100)
+});
